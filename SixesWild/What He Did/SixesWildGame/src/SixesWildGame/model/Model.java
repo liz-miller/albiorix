b53fd17583907cbceee4083d2351e13e01ad2788
Sean McCrone <semccrone@fusion.wpi.edu>
2015-04-29 13:23:08 -0400

    This should be everything I changed. The timer is not showing since I do not know what changes you plan to make to the Lighning class. all my changes are marked by //-- comments. There is one new file in here too.

--- SixesWild/What He Did/SixesWildGame/src/SixesWildGame/model/Model.java
+++ SixesWild/What He Did/SixesWildGame/src/SixesWildGame/model/Model.java
@@ -5,8 +5,14 @@ package SixesWildGame.model;
 public class Model {
 	
 
-	public Level getLevel() {
+	public Level getLevel(int type) { //-- now takes int, can change this as appropriate
 		int[] starThresholds = {1,2,3};
-		return new Puzzle(30, starThresholds, 3, 3);
+		if (type == 2){
+			return new Puzzle(30, starThresholds, 3, 3); //-- this will need to be changed to lightning
+		}
+		else{
+			return new Puzzle(30, starThresholds, 3, 3); //-- puzzle as default
+		}
+
 	}
 }
